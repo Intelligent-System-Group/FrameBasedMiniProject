4a91046fbc897a664ae621521408fa894efa9c04
Rene Angelo Prado <152822689+RenePrado@users.noreply.github.com>
2025-02-22 10:48:52 +0800

    Update MultipleInstances.py

--- MultipleInstances.py
+++ MultipleInstances.py
@@ -1,48 +1,43 @@
-class ElectricVehicle:
-    def __init__(self, motor, battery_type):
-        self.motor = motor
-        self.battery_type = battery_type
+class Entree:
+    def __init__(self, entree_name, type):
+        self.entree_name = entree_name
+        self.type = type
 
-class SolarVehicle:
-    def __init__(self, solar_panel_type, solar_cell_material):
-        self.solar_panel_type = solar_panel_type
-        self.solar_cell_material = solar_cell_material
+class Beverage:
+    def __init__(self, beverage_name, size):
+        self.beverage_name = beverage_name
+        self.size = size
 
-class MuscleVehicle:
-    def __init__(self, number_of_wheels, pedalling_manpower):
-        self.number_of_wheels = number_of_wheels
-        self.pedalling_manpower = pedalling_manpower
+class Dessert:
+    def __init__(self, dessert_name, flavor):
+        self.dessert_name = dessert_name
+        self.flavor = flavor
 
-class MuscleSolarElectricVehicle(ElectricVehicle, SolarVehicle, MuscleVehicle):
-    def __init__(self, motor, battery_type, solar_panel_type, solar_cell_material,
-                 number_of_wheels, pedalling_manpower, weight, top_speed):
-        ElectricVehicle.__init__(self, motor, battery_type)
-        SolarVehicle.__init__(self, solar_panel_type, solar_cell_material)
-        MuscleVehicle.__init__(self, number_of_wheels, pedalling_manpower)
+class MyOrder(Entree, Beverage, Dessert):
+    def __init__(self, entree_name, type, beverage_name, size,
+                 dessert_name, flavor, total_price):
         
-        self.weight = weight
-        self.top_speed = top_speed
+        Entree.__init__(self, entree_name, type)
+        Beverage.__init__(self, beverage_name, size)
+        Dessert.__init__(self, dessert_name, flavor)
+        
+        self.total_price = total_price
 
     def display_info(self):
-        print(f"Motor: {self.motor}")
-        print(f"Battery Type: {self.battery_type}")
-        print(f"Solar Panel Type: {self.solar_panel_type}")
-        print(f"Solar Cell Material: {self.solar_cell_material}")
-        print(f"Number of Wheels: {self.number_of_wheels}")
-        print(f"Pedalling Manpower: {self.pedalling_manpower}")
-        print(f"Weight: {self.weight} kg")
-        print(f"Top Speed: {self.top_speed} km/h")
+        print("I would like to order:")
+        print(f"Main Dish: {self.entree_name} ({self.type})")
+        print(f"Beverage: {self.beverage_name} ({self.size})")
+        print(f"Dessert: {self.dessert_name} ({self.flavor})")
+        print(f"Price: ${self.total_price:.2f}")
 
-DidikMuscleCar = MuscleSolarElectricVehicle(
-    motor="24V DC",
-    battery_type="Sealed lead acid",
-    solar_panel_type="BP140",
-    solar_cell_material="Crystalline silicon",
-    number_of_wheels=4,
-    pedalling_manpower=2,
-    weight=60,
-    top_speed=35
+Order = MyOrder(
+    entree_name="Beef Wellington",
+    type="Meat",
+    beverage_name="Cola",
+    size="Large",  
+    dessert_name="Sticky Toffee Pudding",
+    flavor="Caramel",
+    total_price=365.12  
 )
 
-DidikMuscleCar.display_info()
-
+Order.display_info()
